9549b786683e6e2e091724e1677ad3113f63c547
snatom <72290722+snatom@users.noreply.github.com>
2020-10-03 22:10:49 +0530

    Update AgeGender.py

--- AgeGender.py
+++ AgeGender.py
@@ -6,8 +6,10 @@ import argparse
 
 def getFaceBox(net, frame, conf_threshold=0.7):
     frameOpencvDnn = frame.copy()
-    frameHeight = frameOpencvDnn.shape[0]
-    frameWidth = frameOpencvDnn.shape[1]
+    fh = frameOpencvDnn.shape[0]
+    fw = frameOpencvDnn.shape[1]
+    frameHeight=fh
+    frameWidth=fw
     blob = cv.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
 
     net.setInput(blob)
