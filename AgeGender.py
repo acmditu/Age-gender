9460171857f63b6ff645fb3355523e8d598098f8
Sarab Nidhaan Singh <matharusn@gmail.com>
2020-10-04 10:26:28 +0530

    Update AgeGender.py
    Added a new print statement

--- AgeGender.py
+++ AgeGender.py
@@ -64,6 +64,7 @@ while cv.waitKey(1) < 0:
     frameFace, bboxes = getFaceBox(faceNet, frame)
     if not bboxes:
         print("No face Detected, Checking next frame")
+        print("Sorry Nothing detected")
         continue
 
     for bbox in bboxes:
